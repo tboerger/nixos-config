efce73699941c78ad4a0317be180f9e6ef7689b8
Thomas Boerger <thomas@webhippie.de>
2022-02-20 19:16:57 +0100

    fix: resolve handling for root user

--- machines/modules/users.nix
+++ machines/modules/users.nix
@@ -22,17 +22,17 @@ in
       users = {
         defaultUserShell = pkgs.zsh;
         mutableUsers = false;
-      };
 
-      users = {
-        root = {
-          shell = pkgs.zsh;
-          hashedPassword = "$6$yuwsoikF5utqohar$fdcvq0iXdmiioiRyBGeVZICzQm4nKlv6.pj9AWh13VRCsE07dN9StDnXV0aslIBb0SWRFC4dY5Um2MYiAMfmH0";
-          openssh = {
-            authorizedKeys = {
-              keys = [
-                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINaQYR0/Oj6k1H03kshz2J7rlGCaDSuaGPhhOs9FcZfn thomas@osiris"
-              ];
+        users = {
+          root = {
+            shell = pkgs.zsh;
+            hashedPassword = "$6$yuwsoikF5utqohar$fdcvq0iXdmiioiRyBGeVZICzQm4nKlv6.pj9AWh13VRCsE07dN9StDnXV0aslIBb0SWRFC4dY5Um2MYiAMfmH0";
+            openssh = {
+              authorizedKeys = {
+                keys = [
+                  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINaQYR0/Oj6k1H03kshz2J7rlGCaDSuaGPhhOs9FcZfn thomas@osiris"
+                ];
+              };
             };
           };
         };
