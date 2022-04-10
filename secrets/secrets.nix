022a9755dddbc3f38fcedf70276010924a7b5f8e
Thomas Boerger <thomas@webhippie.de>
2022-04-10 20:58:16 +0200

    fix(secrets) better naming for secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,11 @@
 let
   thomas = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINaQYR0/Oj6k1H03kshz2J7rlGCaDSuaGPhhOs9FcZfn";
   users = [ thomas ];
+
+  utgard = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN02izetkp+Wru4KE0ZASwOcjJfXr3U0H/Q/i0fjdgJ7";
+  systems = [ utgard ];
 in
 {
-  "acme.age".publicKeys = users;
+  "services/acme/credentials.age".publicKeys = users ++ systems;
+  "services/dyndns/password.age".publicKeys = users ++ systems;
 }
