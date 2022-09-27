276f85888955df1ad0956ae8ee8ff328898f4998
Thomas Boerger <thomas@webhippie.de>
2022-09-28 01:13:51 +0200

    feat: add internal ssh hosts

--- profiles/thomas/programs/ssh.nix
+++ profiles/thomas/programs/ssh.nix
@@ -4,6 +4,32 @@
   enable = false;
 
   matchBlocks = {
+    "midgard" = {
+      hostname = "192.168.1.5";
+      port = 22;
+      user = "thomas";
+    };
+    "vanaheim" = {
+      hostname = "192.168.1.6";
+      port = 22;
+      user = "thomas";
+    };
+    "niflheim" = {
+      hostname = "192.168.1.7";
+      port = 22;
+      user = "thomas";
+    };
+    "asgard" = {
+      hostname = "192.168.1.10";
+      port = 22;
+      user = "thomas";
+    };
+    "utgard" = {
+      hostname = "192.168.1.11";
+      port = 22;
+      user = "thomas";
+    };
+
     "asbru" = {
       hostname = "asbru.webhippie.de";
       port = 22;
