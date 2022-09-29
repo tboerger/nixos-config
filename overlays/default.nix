a402b5d21a10a1d6169cc654e2aa77636fffa2c8
Thomas Boerger <thomas@webhippie.de>
2022-09-29 14:52:30 +0200

    feat: override citrix workspace download url

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,3 +1,11 @@
 self: super:
 
-{ }
+{
+  citrix_workspace = super.citrix_workspace.overrideAttrs (old: {
+    src = super.fetchurl {
+      name = "linuxx64-22.7.0.20.tar.gz";
+      url = "https://owncloud.boerger.ws/s/zN0Qn7e4mKuu7Tf/download";
+      hash = "sha256-oX5EeK0+rEsMvJ+3vg26J1g5O6LTtqgrMHT/BTWGxfU=";
+    };
+  });
+}
