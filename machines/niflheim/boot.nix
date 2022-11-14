2cb1ad52fbc8c6aff844e0fcdff0c9de84baf670
Thomas Boerger <thomas@webhippie.de>
2022-11-14 21:45:02 +0100

    fix: another try for nilfheim

--- machines/niflheim/boot.nix
+++ machines/niflheim/boot.nix
@@ -15,7 +15,7 @@
       grub = {
         enable = true;
         version = 2;
-        device = "/dev/disk/by-path/pci-0000:00:1f.2-ata-1.0";
+        devices = [ "/dev/disk/by-path/pci-0000:00:1f.2-ata-1.0" "/dev/disk/by-path/pci-0000:00:1f.2-ata-2.0" ];
         efiSupport = false;
       };
     };
